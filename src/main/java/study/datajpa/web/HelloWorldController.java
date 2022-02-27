06aa97f4aac0ea211f26e6ef377e5af6eef0b357
myeongseob91 <myeongseob91@naver.com>
2022-02-27 23:15:42 +0900

    first commit

---
+++ src/main/java/study/datajpa/web/HelloWorldController.java
@@ -0,0 +1,13 @@
+package study.datajpa.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "HelloWorld";
+    }
+}
